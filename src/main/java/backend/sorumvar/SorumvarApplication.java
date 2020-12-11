49d565e9990ae8703984e1115a47e659e24ca968
samet44 <sametkibriz44@gmail.com>
2020-12-12 00:12:12 +0300

    Generated project

---
+++ src/main/java/backend/sorumvar/SorumvarApplication.java
@@ -0,0 +1,13 @@
+package backend.sorumvar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SorumvarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SorumvarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backend/sorumvar/SorumvarApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.sorumvar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SorumvarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
