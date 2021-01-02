1dfbdf52312d3845b3b0b42173a0e71fc1e860b1
samet44 <sametkibriz44@gmail.com>
2021-01-02 12:15:09 +0300

    Updated

--- src/main/java/backend/sorumvar/controller/QuestionController.java
+++ src/main/java/backend/sorumvar/controller/QuestionController.java
@@ -3,7 +3,12 @@ package backend.sorumvar.controller;
 import backend.sorumvar.model.Question;
 import backend.sorumvar.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
